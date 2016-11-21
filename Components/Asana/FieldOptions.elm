cb93dbb8a6e1290533c2c46b8a86e2eba0ec77c5
Drew Haven <drewhaven@asana.com>
2016-11-21 14:41:00 -0800

    Loads custom fields.

--- Components/Asana/FieldOptions.elm
+++ Components/Asana/FieldOptions.elm
@@ -1,11 +1,20 @@
 module Components.Asana.FieldOptions exposing (Props, Model, Msg, component)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Json.Decode as Json
 
 import Base exposing (..)
 import Components.Asana.Api as Api
 import Components.Asana.Model as Asana
 import Components.Asana.ApiResource as ApiResource
+
+type Target
+    = NoTarget
+    | NameTarget
+    | DescriptionTarget
+    | DueDateTarget
+    | CustomFieldTarget Asana.CustomField
 
 type alias Props =
     { customFields : List Asana.CustomField
@@ -13,10 +22,11 @@ type alias Props =
     }
 
 type alias Model =
-    {}
+    { targets : List Target
+    }
 
-type alias Msg =
-    {}
+type Msg =
+    TargetUpdated Index Target
 
 component : Props -> Component Model Msg
 component props =
@@ -27,14 +37,87 @@ component props =
     }
 
 init : Props -> (Model, Cmd Msg)
-init _ =
-    ({}, Cmd.none)
+init { maxValues } =
+    let
+        targets = List.repeat maxValues NoTarget
+    in
+        ({ targets = targets }, Cmd.none)
 
 update : Props -> Msg -> Model -> (Model, Cmd Msg)
 update props msg model =
     (model, Cmd.none)
 
 view : Props -> Model -> Html Msg
-view props model =
-    text <| toString (props, model)
+view props {targets} =
+    div [ class "FieldOptions" ]
+        (List.indexedMap (viewSelect props.customFields) targets)
 
+getTargets : Model -> List Target
+getTargets = .targets
+
+--------------------------------------------------------------------------------
+-- Private
+
+allTargets : List Asana.CustomField -> List Target
+allTargets customFields =
+    let
+        customFieldTargets = List.map CustomFieldTarget customFields
+        genericTargets =
+            [ NoTarget
+            , NameTarget
+            , DescriptionTarget
+            , DueDateTarget
+            ]
+    in
+        genericTargets ++ customFieldTargets
+
+viewSelect : List Asana.CustomField -> Int -> Target -> Html Msg
+viewSelect customFields index selectedTarget =
+    let
+        targets = allTargets customFields
+        options = List.map (viewOption selectedTarget) targets
+    in
+        select [ class "FieldOptions-select", on "change" (onChange index customFields) ] options
+
+onChange : Index -> List Asana.CustomField -> Json.Decoder Msg
+onChange index customFields =
+    Json.map (TargetUpdated index) <| Json.map (targetFromString customFields) <| Json.at ["target", "value"] Json.string
+
+
+viewOption : Target -> Target -> Html Msg
+viewOption selectedTarget target =
+    option [ selected (selectedTarget == target), value <| targetString target ] [ text <| targetString target ]
+
+targetString : Target -> String
+targetString target =
+    case target of
+        NoTarget ->
+            "None"
+        NameTarget ->
+            "Name"
+        DescriptionTarget ->
+            "Description"
+        DueDateTarget ->
+            "Due Date"
+        CustomFieldTarget customField ->
+            "CF: " ++ customField.name
+
+targetFromString : List Asana.CustomField -> String -> Target
+targetFromString customFields str =
+    case str of
+        "None" ->
+            NoTarget
+        "Name" ->
+            NameTarget
+        "Description" ->
+            DescriptionTarget
+        "Due Date" ->
+            DueDateTarget
+        str ->
+            List.foldr
+                (\customField target ->
+                    if str == "CF: " ++ customField.name
+                        then CustomFieldTarget customField
+                        else target)
+                NoTarget
+                customFields
